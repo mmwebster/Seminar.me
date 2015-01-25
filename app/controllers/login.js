4a8366a607e45c840952f16720dfdb574da5371f
mmwebster <milo.webster@gmail.com>
2015-01-24 18:21:36 -0800

    simple auth working in initial state

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -11,14 +11,7 @@ export default Ember.ObjectController.extend({
       console.log('authenticating user');
 
       // authenticate with linkedin
-      // var jso = new JSO({
-      //   providerID: "google",
-      //   client_id: "541950296471.apps.googleusercontent.com",
-      //   redirect_uri: "http://bridge.uninett.no/jso/index.html",
-      //   authorization: "https://accounts.google.com/o/oauth2/auth",
-      //   scopes: { request: ["https://www.googleapis.com/auth/userinfo.profile"]}
-      // });
-      // debugger
+      
       // debugger
 
       // transition to search
